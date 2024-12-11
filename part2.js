31fbe2c87a18e009b8ad4d802b83d8f53cee9c23
nnh1125 <ngan_ha.nguyen@tufts.edu>
2024-12-10 19:34:54 -0500

    Add files via upload

--- part2.js
+++ part2.js
@@ -10,30 +10,30 @@ const server = http.createServer(async (req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
 
-    if (pathname === "/") {
+    if (pathname == "/") {
         // Serve the home.html file
         fs.readFile("home.html", (err, data) => {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(data);
         });
-    } else if (pathname === "/process") {
+    } else if (pathname == "/process") { //Read the info from the database
         const queryData = parsedUrl.query;
         await client.connect();
         const db = client.db("Stock");
         const collection = db.collection("PublicCompanies");
 
         let results = [];
-        if (queryData.searchType === "ticker") {
+        if (queryData.searchType == "ticker") {
             results = await collection.find({ ticker: queryData.query }).toArray();
-        } else if (queryData.searchType === "name") {
+        } else if (queryData.searchType == "name") {
             results = await collection.find({ name: new RegExp(queryData.query, "i") }).toArray();
         }
 
-        let htmlResponse = "<h1>Search Results</h1><ul>";
+        let htmlResponse = "<h1>Search Results</h1><ul>"; //Response page
         results.forEach(result => {
             htmlResponse += `<li>Name: ${result.name}, Ticker: ${result.ticker}, Price: $${result.price}</li>`;
         });
-        htmlResponse += results.length === 0 ? "<li>No results found.</li>" : "";
+        htmlResponse += results.length == 0 ? "<li>No results found.</li>" : "";
         htmlResponse += "</ul>";
 
         res.writeHead(200, { "Content-Type": "text/html" });
@@ -42,6 +42,4 @@ const server = http.createServer(async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT);
